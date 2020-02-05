f11023a07039d8d1b67ea42baac02e47d8368da3
Roger Robertson Jr <rjrjr@pm.me>
2020-02-05 18:02:54 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,7 +6,7 @@ def load_library(path = "./emoticons.yml")
   source = YAML.load_file(path)
   result = { :get_emoticon => {}, :get_meaning => {}}
   #get_emoticon = {english emoticon => japanese emoji, get_meaning = { japanese emoji => english word
-  binding.pry
+  #binding.pry
   source.each { |emoticons|
     #puts "DEFINITION"
     word = emoticons[0]
@@ -20,23 +20,28 @@ def load_library(path = "./emoticons.yml")
     result[:get_emoticon][eng] = jap
     result[:get_meaning][jap] = word
   }
-  pp result
+  #pp result
   result
 end
 
 def get_japanese_emoticon(file_path, emoji)
   # code goes here
   source = load_library(file_path)
-  
+  source[:get_emoticon].each { |emoticon|
+    if emoticon[0] == emoji
+      #pp emoticon.fetch(emoji)
+      return emoticon[1]
+    end
+  }
   return "Sorry, that emoticon was not found"
 end
 
 def get_english_meaning(file_path, emoji)
   # code goes here
   source = load_library(file_path)
-  source.each { |definition, symbol|
-    if symbol[1] == emoji
-      return definition
+  source[:get_meaning].each { |emoticon|
+    if emoticon[0] == emoji
+      return emoticon[1]
     end
   }
   return "Sorry, that emoticon was not found"
